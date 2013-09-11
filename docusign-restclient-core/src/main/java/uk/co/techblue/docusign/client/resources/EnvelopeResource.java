495e9c5353e5c5b88e112507d75df1f98c729feb
dthompson <dthompson@safmt.org>
2013-09-11 10:32:50 -0600

    Correct code to be able to add an envelope. Add clientUserId parameter where needed for creating embeddable documents.

--- docusign-restclient-core/src/main/java/uk/co/techblue/docusign/client/resources/EnvelopeResource.java
+++ docusign-restclient-core/src/main/java/uk/co/techblue/docusign/client/resources/EnvelopeResource.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2012 Technology Blueprint Ltd
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,6 +38,7 @@ import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataOutput;
 import uk.co.techblue.docusign.client.Resource;
 import uk.co.techblue.docusign.client.dto.CustomField;
 import uk.co.techblue.docusign.client.dto.DocumentFile;
+import uk.co.techblue.docusign.client.dto.Envelope;
 import uk.co.techblue.docusign.client.dto.EnvelopeDetailInfo;
 import uk.co.techblue.docusign.client.dto.EnvelopeDocumentInfo;
 import uk.co.techblue.docusign.client.dto.EnvelopeNotificationInfo;
@@ -51,94 +52,103 @@ import uk.co.techblue.docusign.client.utils.DocuSignConstants;
 @Path(DocuSignConstants.RESOURCE_CONTEXT_PATH)
 public interface EnvelopeResource extends Resource {
 
-	@PUT
-	@Path("envelopes/{envelopeId}/status")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public ClientResponse<String> changeStatus(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId,
-			StatusChangeRequest statusChangeRequest);
+    @PUT
+    @Path("envelopes/{envelopeId}/status")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ClientResponse<String> changeStatus(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId,
+            StatusChangeRequest statusChangeRequest);
 
-	@PUT
-	@Path("envelopes/{envelopeId}/documents/{documentId}")
-	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
-	public ClientResponse<String> addDocumentToDraftEnvelope(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@HeaderParam(FieldName.CONTENT_DISPOSITION) String contentDisposition,
-			@PathParam("envelopeId") String envelopeId,
-			@PathParam("documentId") String documentId, File docFile);
+    @PUT
+    @Path("envelopes/{envelopeId}/documents/{documentId}")
+    @Consumes(MediaType.APPLICATION_OCTET_STREAM)
+    public ClientResponse<String> addDocumentToDraftEnvelope(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @HeaderParam(FieldName.CONTENT_DISPOSITION) String contentDisposition,
+            @PathParam("envelopeId") String envelopeId,
+            @PathParam("documentId") String documentId, File docFile);
 
-	@GET
-	@Path("envelopes/{envelopeId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public ClientResponse<EnvelopeDetailInfo> getEnvelope(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId);
+    @GET
+    @Path("envelopes/{envelopeId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ClientResponse<EnvelopeDetailInfo> getEnvelope(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId);
 
-	@POST
-	@Path("folders/draft")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public ClientResponse<Map<String, String>> saveToDrafts(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			MultipartFormDataOutput formDataOutput);
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("envelopes")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public ClientResponse<Map<String, String>> saveToDrafts(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            MultipartFormDataOutput formDataOutput);
 
-	@GET
-	@Path("envelopes")
-	@Produces(MediaType.APPLICATION_JSON)
-	public ClientResponse<EnvelopeStatusResponse> getEnvelopeStatus(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@Form EnvelopeStatusQueryForm statusQueryForm);
-	
-	@GET
-	@Path("envelopes/{envelopeId}/documents/certificate")
-	public ClientResponse<DocumentFile> getCertificate(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId,
-			@QueryParam("watermark") Boolean watermark,
-			@QueryParam("certificate") Boolean certificate);
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("envelopes")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ClientResponse<Map<String, String>> saveToDrafts(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            Envelope envelope);
 
-	@GET
-	@Path("envelopes/{envelopeId}/documents/combined")
-	public ClientResponse<DocumentFile> getDocumentsCombined(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId);
+    @GET
+    @Path("envelopes")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ClientResponse<EnvelopeStatusResponse> getEnvelopeStatus(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @Form EnvelopeStatusQueryForm statusQueryForm);
 
-	@GET
-	@Path("envelopes/{envelopeId}/documents")
-	@Produces(MediaType.APPLICATION_JSON)
-	public ClientResponse<EnvelopeDocumentInfo> getDocumentsInfo(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId);
+    @GET
+    @Path("envelopes/{envelopeId}/documents/certificate")
+    public ClientResponse<DocumentFile> getCertificate(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId,
+            @QueryParam("watermark") Boolean watermark,
+            @QueryParam("certificate") Boolean certificate);
 
-	@GET
-	@Path("/envelopes/{envelopeId}/documents/{documentId}")
-	public ClientResponse<DocumentFile> getDocument(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId,
-			@PathParam("documentId") String documentId);
-	
-	
-	@GET
-	@Path("/envelopes/{envelopeId}/custom_fields")
-	@Produces(MediaType.APPLICATION_JSON)
-	public ClientResponse<Map<String,List<CustomField>>> getCustomFields(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId);
-	
-	@GET
-	@Path("/envelopes/{envelopeId}/notification")
-	@Produces(MediaType.APPLICATION_JSON)
-	public ClientResponse<EnvelopeNotificationInfo> getNotificationInfo(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId);
-	
-	@GET
-	@Path("/envelopes/{envelopeId}/recipients")
-	@Produces(MediaType.APPLICATION_JSON)
-	public ClientResponse<RecipientStatusCollection> getRecipientStatus(
-			@HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
-			@PathParam("envelopeId") String envelopeId,
-			@QueryParam("include_tabs") Boolean includeTabs,
-			@QueryParam("include_extended") Boolean includeExtended);
+    @GET
+    @Path("envelopes/{envelopeId}/documents/combined")
+    public ClientResponse<DocumentFile> getDocumentsCombined(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId);
+
+    @GET
+    @Path("envelopes/{envelopeId}/documents")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ClientResponse<EnvelopeDocumentInfo> getDocumentsInfo(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId);
+
+    @GET
+    @Path("/envelopes/{envelopeId}/documents/{documentId}")
+    public ClientResponse<DocumentFile> getDocument(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId,
+            @PathParam("documentId") String documentId);
+
+
+    @GET
+    @Path("/envelopes/{envelopeId}/custom_fields")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ClientResponse<Map<String,List<CustomField>>> getCustomFields(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId);
+
+    @GET
+    @Path("/envelopes/{envelopeId}/notification")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ClientResponse<EnvelopeNotificationInfo> getNotificationInfo(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId);
+
+    @GET
+    @Path("/envelopes/{envelopeId}/recipients")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ClientResponse<RecipientStatusCollection> getRecipientStatus(
+            @HeaderParam(DocuSignConstants.HEADER_PARAM_AUTHENTICATION) DocuSignCredentials credentials,
+            @PathParam("envelopeId") String envelopeId,
+            @QueryParam("include_tabs") Boolean includeTabs,
+            @QueryParam("include_extended") Boolean includeExtended);
 
 }
